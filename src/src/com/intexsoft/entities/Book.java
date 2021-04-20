5e089b55cb0c2d5265bad5028e8c525a0d03fbc3
ilya.kovalevich <ilya.kovalevich@intexsoft.by>
2021-04-20 10:04:46 +0300

    Create Book class , Order class , Reader class

--- src/src/com/intexsoft/entities/Book.java
+++ src/src/com/intexsoft/entities/Book.java
@@ -1,4 +1,33 @@
 package com.intexsoft.entities;
 
+import java.util.List;
+
 public class Book {
+    Integer id;
+    String title;
+    String author;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
---
+++ src/src/com/intexsoft/entities/Order.java
@@ -0,0 +1,42 @@
+package com.intexsoft.entities;
+
+import java.util.Date;
+
+public class Order {
+    Integer id;
+    Integer idClient;
+    Date dateStart;
+    Date dateFinish;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(Integer idClient) {
+        this.idClient = idClient;
+    }
+
+    public Date getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(Date dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public Date getDateFinish() {
+        return dateFinish;
+    }
+
+    public void setDateFinish(Date dateFinish) {
+        this.dateFinish = dateFinish;
+    }
+}
