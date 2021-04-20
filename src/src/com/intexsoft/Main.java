3c75b1cbf96b1a60b5ad0373d6d2c25d4ba39d65
ilya.kovalevich <ilya.kovalevich@intexsoft.by>
2021-04-20 11:17:58 +0300

    try get properties by key

--- src/src/com/intexsoft/Main.java
+++ src/src/com/intexsoft/Main.java
@@ -1,4 +1,32 @@
 package com.intexsoft;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.Set;
+
 public class Main {
+    public static final String PATH_PROPERTIES_FILE = "src/src/resources/config.properties";
+
+    public static void main(String[] args) {
+        FileInputStream fileInputStream;
+
+        Properties properties = new Properties();
+        Set keys;
+        String str;
+        try {
+            fileInputStream = new FileInputStream(PATH_PROPERTIES_FILE);
+            properties.load(fileInputStream);
+            keys = properties.keySet();
+            Iterator iterator = keys.iterator();
+            while (iterator.hasNext()) {
+                str = (String) iterator.next();
+                System.out.println(str + " = " + properties.getProperty(str));
+            }
+        } catch (IOException ioException) {
+            System.out.println(ioException.getMessage());
+        }
+    }
 }
